dc0f85b5f8f03108f8f2a527a40561e4209e8eb9
kristofferlarberg <mail@kristofferlarberg.se>
2021-06-21 18:41:14 +0200

    Fix buggy url for pages

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -11,7 +11,7 @@ export const linkResolver = (doc) => {
     if (doc.type === 'homepage') return '/';
     if (doc.type === 'news') return '/nyheter';
     if (doc.type === 'news_post') return `/nyheter/${doc.uid}`;
-    if (doc.type === 'page') return `sidor/${doc.uid}`;
+    if (doc.type === 'page') return `/sidor/${doc.uid}`;
     if (doc.type === 'stores') return '/butiker';
     if (doc.type === 'texts') return '/texter';
     if (doc.type === 'text_post') return `/texter/${doc.uid}`;
